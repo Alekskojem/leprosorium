7f31fefb747d4c06169f1253587fca679e16fdc9
alekskojem <alekskojem@gmail.com>
2017-09-26 21:53:58 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -6,13 +6,19 @@ def init_db
 	@db = SQLite3::Database.new 'leprosorium.db'
 	@db.results_as_hash = true
 end
-
+# before визивается каждий раз при перезагрузке
+# любой страници
 before do
-	init_db
+  # Инициализация БД
+  init_db
 end
-
+# configure визивается каждий рвз при конфигурации приложения:
+# когда изменячется код программи или что то ввели
+# или перезгрузилась страница
 configure do
+  #Инициализауия БД
 	 init_db
+   # Создает таблицу если таблица еще не существует
    @db.execute 'CREATE TABLE IF NOT EXISTS Post
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,6 +27,8 @@ configure do
 )'
 end
 
+# обработчик get запроса /new
+# (браузер получает страницу с сервера)
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -30,6 +38,8 @@ get '/new' do
 end
 
 post '/new' do
+# Получаем переменную из Post запроса /new
+# (браузер отправляет страницу на сервер)
  content = params[:content]
 erb "You taped #{content}"
 end
