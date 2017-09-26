d54d974052bee445c0fdff337d427e1cf8a7902a
Aleksandr Kojemyako <alekskojem@gmail.com>
2017-09-26 11:57:48 +0300

    Add stab for /new

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-
+require 'sqlite3'
 get '/' do
-	"Hello World"
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/new' do
+ erb "Hello World"
 end
